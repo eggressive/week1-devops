8f405e01a389e7793e5f3e8da9846257ec9421ba
Dimitar <47664474+eggressive@users.noreply.github.com>
2023-06-15 16:54:58 +0000

    resolve path to calculator module

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,3 +1,8 @@
+# Neede to resolve path issue to calculator module
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+
 from calculator import add, div, mul, sub
 
 
