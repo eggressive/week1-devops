4979111ff72da7567bcaf593208824f161d3de39
Dimitar <47664474+eggressive@users.noreply.github.com>
2023-06-15 19:05:53 +0000

    test with branch #3

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,7 +3,7 @@ import sys
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
-from calculator import add, div, mul, sub
+from app.calculator import add, div, mul, sub
 
 
 def test_add():
